32a89f8933e83d09f53e1f7aef1f8c12f1a0e238
Rosalinda Rojas <rosalindarojas@college.harvard.edu>
2026-04-06 23:10:42 +0000

    Mon, Apr 6, 2026, 7:10 PM -04:00

---
+++ prototype.py
@@ -0,0 +1,14 @@
+##Single Player Hangman Game
+
+##Create Words List
+words = ['cosmo', 'wanda', 'waldo', 'odlaw', 'cat', 'hat', 'tuple', 'roshambo', 'list', 'coarsen']
+def main():
+    print('## WELCOME TO HANGMAN! ## \n')
+
+# Grab a guess from the player
+        while True:
+            guess = str(input('Please pick your letter: '))
+            if guess :
+                break
+            else:
+                print('')
