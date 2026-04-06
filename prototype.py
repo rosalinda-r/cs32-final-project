4b8670bf1e6666cde69a4e786f63d926ea9d8663
Rosalinda Rojas <rosalindarojas@college.harvard.edu>
2026-04-06 23:14:03 +0000

    Mon, Apr 6, 2026, 7:14 PM -04:00

--- prototype.py
+++ prototype.py
@@ -1,14 +1,50 @@
 ##Single Player Hangman Game
-
+import random
 ##Create Words List
 words = ['cosmo', 'wanda', 'waldo', 'odlaw', 'cat', 'hat', 'tuple', 'roshambo', 'list', 'coarsen']
 def main():
     print('## WELCOME TO HANGMAN! ## \nPress ENTER to START')
+# Pick a random word
 
-# Grab a guess from the player
-        while True:
-            guess = str(input('Please pick your letter: '))
-            if guess :
-                break
-            else:
-                print('')
+word = random.choice(words)
+guessed_letters = []
+wrong_guesses = 0
+max_wrong = 6
+
+# Display word with blanks
+display_word = ["_"] * len(word)
+
+print("Welcome to Hangman!")
+
+while wrong_guesses < max_wrong and "_" in display_word:
+    print("\nWord: " + " ".join(display_word))
+    print("Guessed letters:", guessed_letters)
+    print("Wrong guesses left:", max_wrong - wrong_guesses)
+
+    guess = input("Guess a letter: ").lower()
+
+    # Check input
+    if len(guess) != 1 or not guess.isalpha():
+        print("Please enter a single letter.")
+        continue
+
+    if guess in guessed_letters:
+        print("You already guessed that letter.")
+        continue
+
+    guessed_letters.append(guess)
+
+    if guess in word:
+        print("Correct!")
+        for i in range(len(word)):
+            if word[i] == guess:
+                display_word[i] = guess
+    else:
+        print("Wrong!")
+        wrong_guesses += 1
+
+# End of game
+if "_" not in display_word:
+    print("\nYou won! The word was:", word)
+else:
+    print("\nYou lost! The word was:", word)
