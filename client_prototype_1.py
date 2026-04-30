726bd5ff0cebe3451b3e7960e5c030dc0dbd336e
Rosalinda Rojas <rosalindarojas@college.harvard.edu>
2026-04-30 20:09:52 +0000

    Thu, Apr 30, 2026, 4:09 PM -04:00

--- client_prototype_1.py
+++ client_prototype_1.py
@@ -8,3 +8,111 @@ data = json.loads(client.recv())
 word = data["word"]
 
 
+display = [
+    "_" if char.isalpha() else char
+    for char in word
+]
+
+hangman_stages = [
+    """
+     -----
+     |   |
+         |
+         |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+         |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+     |   |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|   |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\\  |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\\  |
+    /    |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\\  |
+    / \\  |
+         |
+    --------
+    """
+]
+
+guessed = []
+wrong = 0
+
+print("Welcome to Hangman!")
+
+while wrong < 6 and "_" in display:
+    print(hangman_stages[wrong])
+    print("Word:", " ".join(display))
+    print("Guessed letters:", " ".join(guessed))
+
+    guess = input("Enter a letter: ").lower()
+
+    if len(guess) != 1 or not guess.isalpha():
+        print("Enter one valid letter.")
+        continue
+
+    if guess in guessed:
+        print("You already guessed that.")
+        continue
+
+    guessed.append(guess)
+
+    if guess in [c for c in word if c.isalpha()]:
+        for i in range(len(word)):
+            if word[i] == guess:
+                display[i] = guess
+        print("Good guess!")
+    else:
+        wrong += 1
+        print("Wrong guess!")
+
+if "_" not in display:
+    print("\nYou won! The word was", word)
+else:
+    print(hangman_stages[wrong])
+    print("\nYou lost! The word was", word)
+
