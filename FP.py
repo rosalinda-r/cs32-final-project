c7659b76e0483b58535e3168057004b0db76efc5
Rosalinda Rojas <rosalindarojas@college.harvard.edu>
2026-04-30 19:30:28 +0000

    Thu, Apr 30, 2026, 3:30 PM -04:00

---
+++ FP.py
@@ -0,0 +1,152 @@
+import re
+import requests
+
+# Get a random Wikipedia page
+url = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+
+headers = {
+    "User-Agent": "HangmanProject/1.0 (student project)"
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code != 200:
+    print("Error fetching data:", response.status_code)
+    print(response.text[:200])
+    exit()
+
+try:
+    data = response.json()
+except requests.exceptions.JSONDecodeError:
+    print("Invalid response from Wikipedia")
+    print(response.text)
+    exit()
+
+# Extract the title and store it in 'title'
+title = data["title"]
+
+word = title.lower()
+
+###NEW CODE
+def get_book():
+    
+
+mode = input("Choose mode: ")
+
+if mode == "movie":
+    word = get_movie()
+elif mode == "song":
+    word = get_song()
+elif mode == "book":
+    word = get_book()
+else:
+    word = get_wiki()
+####
+
+display = [
+    "_" if char.isalpha() else char
+    for char in word
+]
+
+hangman_stages = [
+    """
+     -----
+     |   |
+         |
+         |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+         |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+     |   |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|   |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\\  |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\\  |
+    /    |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\\  |
+    / \\  |
+         |
+    --------
+    """
+]
+
+guessed = []
+wrong = 0
+
+print("Welcome to Hangman!")
+
+while wrong < 6 and "_" in display:
+    print(hangman_stages[wrong])
+    print("Word:", " ".join(display))
+    print("Guessed letters:", " ".join(guessed))
+
+    guess = input("Enter a letter: ").lower()
+
+    if len(guess) != 1 or not guess.isalpha():
+        print("Enter one valid letter.")
+        continue
+
+    if guess in guessed:
+        print("You already guessed that.")
+        continue
+
+    guessed.append(guess)
+
+    if guess in [c for c in word if c.isalpha()]:
+        for i in range(len(word)):
+            if word[i] == guess:
+                display[i] = guess
+        print("Good guess!")
+    else:
+        wrong += 1
+        print("Wrong guess!")
+
+if "_" not in display:
+    print("\nYou won! The word was", word)
+else:
+    print(hangman_stages[wrong])
+    print("\nYou lost! The word was", word)
